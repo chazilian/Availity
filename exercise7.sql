cab2b474fc55d14de34328efb76b88c7b30a1285
Foo <stevefoo13@gmail.com>
2022-09-23 14:15:11 -0400

    Change to exercise 7. Multiplied cost and quantity to get total cost.

--- exercise7.sql
+++ exercise7.sql
@@ -7,14 +7,14 @@ WHERE LastName like 's%'
 ORDER BY FirstName DESC, LastName DESC
 
 --b
-SELECT c.CustID, c.FirstName, c.LastName, COALESCE(SUM(ol.Cost), 0) as totalOrder
+SELECT c.CustID, c.FirstName, c.LastName, COALESCE(SUM(ol.Cost * ol.Quantity), 0) as totalOrder
 FROM Customer c
 LEFT JOIN Order o ON c.CustID = o.CustomerID AND o.OrderDate >= DATEADD(month, -6, GETDATE()) 
 LEFT JOIN orderLine ol ON ol.OrdID = o.OrderID
 GROUP BY c.CustID, c.FirstName, c.LastName
 
 --c
-SELECT c.CustID, c.FirstName, c.LastName, COALESCE(SUM(ol.Cost), 0) as totalOrder
+SELECT c.CustID, c.FirstName, c.LastName, COALESCE(SUM(ol.Cost * ol.Quantity), 0) as totalOrder
 INTO #CustomersTotalOrder
 FROM Customer c
 LEFT JOIN Order o ON c.CustID = o.CustomerID AND o.OrderDate >= DATEADD(month, -6, GETDATE()) 
